1a690385821c94fdc03c19ea799f4c0ecc36f041
judongseok <wnehdtjr5@gmail.com>
2021-08-11 22:52:54 +0900

    change css

--- Sources/MyBlog/MyTheme.swift
+++ Sources/MyBlog/MyTheme.swift
@@ -1,42 +1,44 @@
-
 import Plot
 import Publish
-import Foundation
 
 public extension Theme {
     /// The default "Foundation" theme that Publish ships with, a very
     /// basic theme mostly implemented for demonstration purposes.
     static var MyTheme: Self {
         Theme(
-            htmlFactory: FoundationHTMLFactory(),
-            resourcePaths: ["Resources/MojitoBlog/styles.css"]
+            htmlFactory: MyThemeHTMLFactory(),
+            resourcePaths: [
+                "Resources/MojitoBlog/styles.css",
+            ]
         )
     }
 }
 
-private struct FoundationHTMLFactory<Site: Website>: HTMLFactory {
+private struct MyThemeHTMLFactory<Site: Website>: HTMLFactory {
     func makeIndexHTML(for index: Index,
                        context: PublishingContext<Site>) throws -> HTML {
         HTML(
             .lang(context.site.language),
             .head(for: index, on: context.site),
-            .body {
-                SiteHeader(context: context, selectedSelectionID: nil)
-                Wrapper {
-                    H1(index.title)
-                    Paragraph(context.site.description)
-                        .class("description")
-                    H2("Latest content")
-                    ItemList(
-                        items: context.allItems(
+            .body(
+                .header(for: context, selectedSection: nil),
+                .wrapper(
+                    .h1(.text(index.title)),
+                    .p(
+                        .class("description"),
+                        .text(context.site.description)
+                    ),
+                    .h2("Latest content"),
+                    .itemList(
+                        for: context.allItems(
                             sortedBy: \.date,
                             order: .descending
                         ),
-                        site: context.site
+                        on: context.site
                     )
-                }
-                SiteFooter()
-            }
+                ),
+                .footer(for: context.site)
+            )
         )
     }
 
@@ -45,14 +47,14 @@ private struct FoundationHTMLFactory<Site: Website>: HTMLFactory {
         HTML(
             .lang(context.site.language),
             .head(for: section, on: context.site),
-            .body {
-                SiteHeader(context: context, selectedSelectionID: section.id)
-                Wrapper {
-                    H1(section.title)
-                    ItemList(items: section.items, site: context.site)
-                }
-                SiteFooter()
-            }
+            .body(
+                .header(for: context, selectedSection: section.id),
+                .wrapper(
+                    .h1(.text(section.title)),
+                    .itemList(for: section.items, on: context.site)
+                ),
+                .footer(for: context.site)
+            )
         )
     }
 
@@ -60,20 +62,23 @@ private struct FoundationHTMLFactory<Site: Website>: HTMLFactory {
                       context: PublishingContext<Site>) throws -> HTML {
         HTML(
             .lang(context.site.language),
-            .head(for: item, on: context.site),
+            .head(for: item,
+                  on: context.site,
+                  stylesheetPaths: ["/styles.css", "/sundellsColors.css"]),
             .body(
                 .class("item-page"),
-                .components {
-                    SiteHeader(context: context, selectedSelectionID: item.sectionID)
-                    Wrapper {
-                        Article {
-                            Div(item.content.body).class("content")
-                            Span("Tagged with: ")
-                            ItemTagList(item: item, site: context.site)
-                        }
-                    }
-                    SiteFooter()
-                }
+                .header(for: context, selectedSection: item.sectionID),
+                .wrapper(
+                    .article(
+                        .div(
+                            .class("content"),
+                            .contentBody(item.body)
+                        ),
+                        .span("Tagged with: "),
+                        .tagList(for: item, on: context.site)
+                    )
+                ),
+                .footer(for: context.site)
             )
         )
     }
@@ -83,11 +88,11 @@ private struct FoundationHTMLFactory<Site: Website>: HTMLFactory {
         HTML(
             .lang(context.site.language),
             .head(for: page, on: context.site),
-            .body {
-                SiteHeader(context: context, selectedSelectionID: nil)
-                Wrapper(page.body)
-                SiteFooter()
-            }
+            .body(
+                .header(for: context, selectedSection: nil),
+                .wrapper(.contentBody(page.body)),
+                .footer(for: context.site)
+            )
         )
     }
 
@@ -96,22 +101,25 @@ private struct FoundationHTMLFactory<Site: Website>: HTMLFactory {
         HTML(
             .lang(context.site.language),
             .head(for: page, on: context.site),
-            .body {
-                SiteHeader(context: context, selectedSelectionID: nil)
-                Wrapper {
-                    H1("Browse all tags")
-                    List(page.tags.sorted()) { tag in
-                        ListItem {
-                            Link(tag.string,
-                                 url: context.site.path(for: tag).absoluteString
+            .body(
+                .header(for: context, selectedSection: nil),
+                .wrapper(
+                    .h1("Browse all tags"),
+                    .ul(
+                        .class("all-tags"),
+                        .forEach(page.tags.sorted()) { tag in
+                            .li(
+                                .class("tag"),
+                                .a(
+                                    .href(context.site.path(for: tag)),
+                                    .text(tag.string)
+                                )
                             )
                         }
-                        .class("tag")
-                    }
-                    .class("all-tags")
-                }
-                SiteFooter()
-            }
+                    )
+                ),
+                .footer(for: context.site)
+            )
         )
     }
 
@@ -120,111 +128,100 @@ private struct FoundationHTMLFactory<Site: Website>: HTMLFactory {
         HTML(
             .lang(context.site.language),
             .head(for: page, on: context.site),
-            .body {
-                SiteHeader(context: context, selectedSelectionID: nil)
-                Wrapper {
-                    H1 {
-                        Text("Tagged with ")
-                        Span(page.tag.string).class("tag")
-                    }
-
-                    Link("Browse all tags",
-                        url: context.site.tagListPath.absoluteString
-                    )
-                    .class("browse-all")
-
-                    ItemList(
-                        items: context.items(
+            .body(
+                .header(for: context, selectedSection: nil),
+                .wrapper(
+                    .h1(
+                        "Tagged with ",
+                        .span(.class("tag"), .text(page.tag.string))
+                    ),
+                    .a(
+                        .class("browse-all"),
+                        .text("Browse all tags"),
+                        .href(context.site.tagListPath)
+                    ),
+                    .itemList(
+                        for: context.items(
                             taggedWith: page.tag,
                             sortedBy: \.date,
                             order: .descending
                         ),
-                        site: context.site
+                        on: context.site
                     )
-                }
-                SiteFooter()
-            }
+                ),
+                .footer(for: context.site)
+            )
         )
     }
 }
 
-private struct Wrapper: ComponentContainer {
-    @ComponentBuilder var content: ContentProvider
-
-    var body: Component {
-        Div(content: content).class("wrapper")
-    }
-}
-
-private struct SiteHeader<Site: Website>: Component {
-    var context: PublishingContext<Site>
-    var selectedSelectionID: Site.SectionID?
-
-    var body: Component {
-        Header {
-            Wrapper {
-                Link(context.site.name, url: "/")
-                    .class("site-name")
-
-                if Site.SectionID.allCases.count > 1 {
-                    navigation
-                }
-            }
-        }
+private extension Node where Context == HTML.BodyContext {
+    static func wrapper(_ nodes: Node...) -> Node {
+        .div(.class("wrapper"), .group(nodes))
     }
 
-    private var navigation: Component {
-        Navigation {
-            List(Site.SectionID.allCases) { sectionID in
-                let section = context.sections[sectionID]
+    static func header<T: Website>(
+        for context: PublishingContext<T>,
+        selectedSection: T.SectionID?
+    ) -> Node {
+        let sectionIDs = T.SectionID.allCases
 
-                return Link(section.title,
-                    url: section.path.absoluteString
+        return .header(
+            .wrapper(
+                .a(.class("site-name"), .href("/"), .text(context.site.name)),
+                .if(sectionIDs.count > 1,
+                    .nav(
+                        .ul(.forEach(sectionIDs) { section in
+                            .li(.a(
+                                .class(section == selectedSection ? "selected" : ""),
+                                .href(context.sections[section].path),
+                                .text(context.sections[section].title)
+                            ))
+                        })
+                    )
                 )
-                .class(sectionID == selectedSelectionID ? "selected" : "")
-            }
-        }
+            )
+        )
     }
-}
 
-private struct ItemList<Site: Website>: Component {
-    var items: [Item<Site>]
-    var site: Site
-
-    var body: Component {
-        List(items) { item in
-            Article {
-                H1(Link(item.title, url: item.path.absoluteString))
-                ItemTagList(item: item, site: site)
-                Paragraph(item.description)
+    static func itemList<T: Website>(for items: [Item<T>], on site: T) -> Node {
+        return .ul(
+            .class("item-list"),
+            .forEach(items) { item in
+                .li(.article(
+                    .h1(.a(
+                        .href(item.path),
+                        .text(item.title)
+                    )),
+                    .tagList(for: item, on: site),
+                    .p(.text(item.description))
+                ))
             }
-        }
-        .class("item-list")
+        )
     }
-}
 
-private struct ItemTagList<Site: Website>: Component {
-    var item: Item<Site>
-    var site: Site
-
-    var body: Component {
-        List(item.tags) { tag in
-            Link(tag.string, url: site.path(for: tag).absoluteString)
-        }
-        .class("tag-list")
+    static func tagList<T: Website>(for item: Item<T>, on site: T) -> Node {
+        return .ul(.class("tag-list"), .forEach(item.tags) { tag in
+            .li(.a(
+                .href(site.path(for: tag)),
+                .text(tag.string)
+            ))
+        })
     }
-}
 
-private struct SiteFooter: Component {
-    var body: Component {
-        Footer {
-            Paragraph {
-                Text("Generated using ")
-                Link("Publish", url: "https://github.com/johnsundell/publish")
-            }
-            Paragraph {
-                Link("RSS feed", url: "/feed.rss")
-            }
-        }
+    static func footer<T: Website>(for site: T) -> Node {
+        return .footer(
+            .p(
+                .text("Generated using "),
+                .a(
+                    .text("Publish"),
+                    .href("https://github.com/johnsundell/publish")
+                )
+            ),
+            .p(.a(
+                .text("RSS feed"),
+                .href("/feed.rss")
+            ))
+        )
     }
 }
